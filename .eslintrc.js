abdc471ec8dd26734fa52bfa027da1ec299d6c15
João Lucas Stephan <lucastephann@gmail.com>
2022-09-28 19:12:46 -0300

    Project structure and database configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,6 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    tsconfigRootDir : __dirname, 
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
@@ -15,11 +14,11 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'prefer-rest-params': 'off',
   },
-};
+}
